be10baa276389bcb5bb28d6d866ed31f93d85c23
Jack Ambery <jack.ambery@gmail.com>
2023-06-25 11:57:03 -0400

    completed inital stages of tabs animation

--- DashboardApp/src/app/app-routing.module.ts
+++ DashboardApp/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bookmarks', component: BookmarksComponent}
+];
+
+// left off at ep. 2 41:00
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
